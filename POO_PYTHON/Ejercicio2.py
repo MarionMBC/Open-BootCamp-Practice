1faf1233ad6ac30a51ea34407d2991d83637ddde
MarionMBC <marion.bustamante@unah.hn>
2022-11-02 20:25:12 -0600

    Ejercicio 2 terminado

---
+++ POO_PYTHON/Ejercicio2.py
@@ -0,0 +1,22 @@
+# En este segundo ejercicio, tendréis que crear un programa que tenga una clase llamada
+# Alumno que tenga como atributos su nombre y su nota. Deberéis de definir los métodos
+# para inicializar sus atributos, imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no.
+
+class Alumno:
+    nombre = ""
+    nota = 0
+
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+
+    def isApproved(self):
+        return "aprobó" if self.nota>=7 else"reprobó"
+
+    def __str__(self):
+        print(f'El alumno: {self.nombre} {self.isApproved()} la clase.')
+
+
+Alumno1 = Alumno("Marion Bustamante", 7)
+
+Alumno1.__str__()
